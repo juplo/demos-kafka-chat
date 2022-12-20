5ede43897420ecc3c285446076e3838bc46126d2
Kai Moritz <kai@juplo.de>
2022-12-20 18:26:15 +0100

    refactor: Simplified the name of the application-class

--- src/main/java/de/juplo/kafka/chatroom/ChatroomApplication.java
+++ src/main/java/de/juplo/kafka/chatroom/ChatroomApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatroomServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ChatroomServiceApplication.class, args);
+public class ChatroomApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ChatroomApplication.class, args);
 	}
 
 }
