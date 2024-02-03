9aa68af63307c5d1ddadb0f0c9038a603d12947c
Kai Moritz <kai@juplo.de>
2024-02-03 20:43:29 +0100

    refactor: `KafkaServicesApplicationRunner` aufgeräumt

--- src/main/java/de/juplo/kafka/chat/backend/implementation/kafka/KafkaServicesApplicationRunner.java
+++ src/main/java/de/juplo/kafka/chat/backend/implementation/kafka/KafkaServicesApplicationRunner.java
@@ -2,7 +2,6 @@ package de.juplo.kafka.chat.backend.implementation.kafka;
 
 import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -15,14 +14,13 @@ import org.springframework.stereotype.Component;
     havingValue = "kafka")
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class KafkaServicesApplicationRunner implements ApplicationRunner
 {
   private final ConsumerTaskRunner consumerTaskRunner;
 
 
   @Override
-  public void run(ApplicationArguments args) throws Exception
+  public void run(ApplicationArguments args)
   {
     consumerTaskRunner.executeConsumerTasks();
   }
