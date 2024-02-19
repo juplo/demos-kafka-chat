5ad9003cb15c3c8a1870a580da3d242bb8b7de1a
Kai Moritz <kai@juplo.de>
2024-02-19 11:05:17 +0100

    test: Introduced integration-tests for `NoStorageStorageStrategy` -- ALIGN

--- src/test/java/de/juplo/kafka/chat/backend/InMemoryWithNoStorageAndShardingConfigurationIT.java
+++ src/test/java/de/juplo/kafka/chat/backend/InMemoryWithNoStorageAndShardingConfigurationIT.java
@@ -1,16 +1,31 @@
 package de.juplo.kafka.chat.backend;
 
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = {
-        "chat.backend.inmemory.storage-strategy=files",
-        "chat.backend.inmemory.storage-directory=target/test-classes/data/files",
+        "chat.backend.inmemory.storage-strategy=none",
         "chat.backend.inmemory.sharding-strategy=kafkalike",
         "chat.backend.inmemory.num-shards=10",
         "chat.backend.inmemory.owned-shards=2" })
-class InMemoryWithFilesAndShardingConfigurationIT extends AbstractConfigurationWithShardingIT
+class InMemoryWithNoStorageAndShardingConfigurationIT extends AbstractConfigurationWithShardingIT
 {
+  @Override
+  @Disabled("Chat-Rooms cannot be restored, if storage is disabled")
+  void testRestoredChatRoomsCanBeListed() {}
+
+  @Override
+  @Disabled("Chat-Rooms cannot be restored, if storage is disabled")
+  void testRestoredChatRoomHasExpectedDetails() {}
+
+  @Override
+  @Disabled("Chat-Rooms cannot be restored, if storage is disabled")
+  void testRestoredMessageForUteHasExpectedText() {}
+
+  @Override
+  @Disabled("Chat-Rooms cannot be restored, if storage is disabled")
+  void testRestoredMessageForPeterHasExpectedText() {}
 }
--- src/test/java/de/juplo/kafka/chat/backend/InMemoryWithNoStorageConfigurationIT.java
+++ src/test/java/de/juplo/kafka/chat/backend/InMemoryWithNoStorageConfigurationIT.java
@@ -1,5 +1,6 @@
 package de.juplo.kafka.chat.backend;
 
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
@@ -7,8 +8,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
 		properties = {
 				"chat.backend.inmemory.sharding-strategy=none",
-				"chat.backend.inmemory.storage-strategy=files",
-				"chat.backend.inmemory.storage-directory=target/test-classes/data/files" })
-class InMemoryWithFilesConfigurationIT extends AbstractConfigurationIT
+				"chat.backend.inmemory.storage-strategy=none" })
+class InMemoryWithNoStorageConfigurationIT extends AbstractConfigurationIT
 {
+	@Override
+	@Disabled("Chat-Rooms cannot be restored, if storage is disabled")
+	void testRestoredChatRoomsCanBeListed() {}
+
+	@Override
+	@Disabled("Chat-Rooms cannot be restored, if storage is disabled")
+	void testRestoredChatRoomHasExpectedDetails() {}
+
+	@Override
+	@Disabled("Chat-Rooms cannot be restored, if storage is disabled")
+	void testRestoredMessageForUteHasExpectedText() {}
+
+	@Override
+	@Disabled("Chat-Rooms cannot be restored, if storage is disabled")
+	void testRestoredMessageForPeterHasExpectedText() {}
 }
