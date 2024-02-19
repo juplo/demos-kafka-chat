ec286994cade68a495b25a8a8700f076447b4337
Kai Moritz <kai@juplo.de>
2024-02-19 11:01:13 +0100

    test: Introduced integration-tests for `NoStorageStorageStrategy` -- COPY

---
+++ src/test/java/de/juplo/kafka/chat/backend/InMemoryWithNoStorageConfigurationIT.java
@@ -0,0 +1,14 @@
+package de.juplo.kafka.chat.backend;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		properties = {
+				"chat.backend.inmemory.sharding-strategy=none",
+				"chat.backend.inmemory.storage-strategy=files",
+				"chat.backend.inmemory.storage-directory=target/test-classes/data/files" })
+class InMemoryWithFilesConfigurationIT extends AbstractConfigurationIT
+{
+}
