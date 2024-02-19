da1635cb228c5b587c9329214f658eb6bbda8647
Kai Moritz <kai@juplo.de>
2024-02-19 14:31:21 +0100

    feat: Added log message for disabled storage

--- src/main/java/de/juplo/kafka/chat/backend/storage/nostorage/NoStorageStorageStrategy.java
+++ src/main/java/de/juplo/kafka/chat/backend/storage/nostorage/NoStorageStorageStrategy.java
@@ -1,15 +1,24 @@
 package de.juplo.kafka.chat.backend.storage.nostorage;
 
+import de.juplo.kafka.chat.backend.domain.ChatHomeService;
 import de.juplo.kafka.chat.backend.domain.ChatRoomInfo;
 import de.juplo.kafka.chat.backend.domain.Message;
 import de.juplo.kafka.chat.backend.implementation.StorageStrategy;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
 import java.util.UUID;
 
 
+@Slf4j
 public class NoStorageStorageStrategy implements StorageStrategy
 {
+  @Override
+  public void write(ChatHomeService chatHomeService)
+  {
+    log.info("Storage is disabled: Not storing {}", chatHomeService);
+  }
+
   @Override
   public void writeChatRoomInfo(Flux<ChatRoomInfo> chatRoomInfoFlux) {}
 
