ee98f1d825750c86fb159036ff9fc751fee8f7a7
Kai Moritz <kai@juplo.de>
2024-02-22 14:24:28 +0100

    test: Simplified `ChatHomeServiceTest`
    * Removed the unnecessary explicit instantiation of the `ObjectMapper`.

--- src/test/java/de/juplo/kafka/chat/backend/domain/ChatHomeServiceTest.java
+++ src/test/java/de/juplo/kafka/chat/backend/domain/ChatHomeServiceTest.java
@@ -1,7 +1,5 @@
 package de.juplo.kafka.chat.backend.domain;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import de.juplo.kafka.chat.backend.ChatBackendProperties;
 import de.juplo.kafka.chat.backend.domain.exceptions.LoadInProgressException;
 import de.juplo.kafka.chat.backend.domain.exceptions.UnknownChatroomException;
@@ -79,14 +77,6 @@ public abstract class ChatHomeServiceTest
 
   static class TestConfiguration
   {
-    @Bean
-    ObjectMapper objectMapper()
-    {
-      ObjectMapper objectMapper = new ObjectMapper();
-      objectMapper.registerModule(new JavaTimeModule());
-      return objectMapper;
-    }
-
     @Bean
     Clock clock()
     {
