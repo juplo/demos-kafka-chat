99b7a91a74e9c0e064e2592c3364a527a32a2460
Kai Moritz <kai@juplo.de>
2024-03-20 18:29:45 +0100

    test: HandoverIT-POC - Splitted up code into smaller classes -- ALIGN

--- src/test/java/de/juplo/kafka/chat/backend/AbstractHandoverITContainers.java
+++ src/test/java/de/juplo/kafka/chat/backend/AbstractHandoverITContainers.java
@@ -1,19 +1,8 @@
 package de.juplo.kafka.chat.backend;
 
-import de.juplo.kafka.chat.backend.api.ChatRoomInfoTo;
-import de.juplo.kafka.chat.backend.api.MessageTo;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.awaitility.Awaitility;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
@@ -21,49 +10,37 @@ import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.ImagePullPolicy;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.util.retry.Retry;
 
-import java.io.IOException;
 import java.time.Duration;
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
 
-@Testcontainers
 @Slf4j
-public abstract class AbstractHandoverIT
+public abstract class AbstractHandoverITContainers
 {
   static final ImagePullPolicy NEVER_PULL = imageName -> false;
 
 
-  @Test
-  void test() throws InterruptedException
-  {
-    ChatRoomInfoTo chatRoom = createChatRoom("bar").block();
-    User user = new User("nerd");
-    IntStream
-        .rangeClosed(1,100)
-        .mapToObj(i ->sendMessage(chatRoom, user, "Message #" + i))
-        .map(result -> result
-            .map(MessageTo::toString)
-            .retryWhen(Retry.fixedDelay(10, Duration.ofSeconds(1)))
-            .block())
-        .forEach(result -> log.info("{}", result));
+  final Network network = Network.newNetwork();
+  final GenericContainer haproxy, backend1, backend2, backend3;
 
-    receiveMessages(chatRoom)
-        .take(100)
-        .doOnNext(message -> log.info("message: {}", message))
-        .then()
-        .block();
+
+  AbstractHandoverITContainers()
+  {
+    haproxy = createHaproxyContainer();
+    backend1 = createBackendContainer("1");
+    backend2 = createBackendContainer("2");
+    backend3 = createBackendContainer("3");
   }
 
 
-  abstract void setUpExtra() throws IOException, InterruptedException;
+  void setUpExtra() throws Exception
+  {
+    log.info("This setup does not need any extra containers");
+  }
 
-  @BeforeEach
   void setUp() throws Exception
   {
     setUpExtra();
@@ -126,82 +103,40 @@ public abstract class AbstractHandoverIT
             .block());
   }
 
-  Network network = Network.newNetwork();
+  abstract String[] getBackendCommand();
 
-  GenericContainer haproxy =
-      new GenericContainer(DockerImageName.parse("haproxytech/haproxy-debian:2.8"))
-          .withNetwork(network)
-          .withNetworkAliases("haproxy")
-          .withClasspathResourceMapping(
-              "haproxy.cfg",
-              "/usr/local/etc/haproxy/haproxy.cfg",
-              BindMode.READ_ONLY)
-          .withClasspathResourceMapping(
-              "sharding.map",
-              "/usr/local/etc/haproxy/sharding.map",
-              BindMode.READ_WRITE)
-          .withExposedPorts(8400, 8401, 8404)
-          .withLogConsumer(new Slf4jLogConsumer(log, true).withPrefix("HAPROXY"));
-
-  abstract String[] getCommandBackend1();
-  GenericContainer backend1 =
-      new GenericContainer(DockerImageName.parse("juplo/chat-backend:0.0.1-SNAPSHOT"))
-          .withImagePullPolicy(NEVER_PULL)
-          .withNetwork(network)
-          .withNetworkAliases("backend-1")
-          .withCommand(getCommandBackend1())
-          .withExposedPorts(8080)
-          .waitingFor(Wait.forLogMessage(".*Started\\ ChatBackendApplication.*\\n", 1))
-          .withLogConsumer(new Slf4jLogConsumer(log, true).withPrefix("BACKEND-1"));
-
-  abstract String[] getCommandBackend2();
-  GenericContainer backend2 =
-      new GenericContainer(DockerImageName.parse("juplo/chat-backend:0.0.1-SNAPSHOT"))
-          .withImagePullPolicy(NEVER_PULL)
-          .withNetwork(network)
-          .withNetworkAliases("backend-2")
-          .withCommand(getCommandBackend2())
-          .withExposedPorts(8080)
-          .waitingFor(Wait.forLogMessage(".*Started\\ ChatBackendApplication.*\\n", 1))
-          .withLogConsumer(new Slf4jLogConsumer(log, true).withPrefix("BACKEND-2"));
-
-  abstract String[] getCommandBackend3();
-  GenericContainer backend3 =
-      new GenericContainer(DockerImageName.parse("juplo/chat-backend:0.0.1-SNAPSHOT"))
-          .withImagePullPolicy(NEVER_PULL)
-          .withNetwork(network)
-          .withNetworkAliases("backend-3")
-          .withCommand(getCommandBackend3())
-          .withExposedPorts(8080)
-          .waitingFor(Wait.forLogMessage(".*Started\\ ChatBackendApplication.*\\n", 1))
-          .withLogConsumer(new Slf4jLogConsumer(log, true).withPrefix("BACKEND-3"));
-
-
-  @EqualsAndHashCode
-  @ToString
-  class User
+  final GenericContainer createHaproxyContainer()
   {
-    @Getter
-    private final String name;
-    private int serial = 0;
-
-
-    User (String name)
-    {
-      this.name = name;
-    }
-
-
-    int nextSerial()
-    {
-      return ++serial;
-    }
+    return new GenericContainer(DockerImageName.parse("haproxytech/haproxy-debian:2.8"))
+        .withNetwork(network)
+        .withNetworkAliases("haproxy")
+        .withClasspathResourceMapping(
+            "haproxy.cfg",
+            "/usr/local/etc/haproxy/haproxy.cfg",
+            BindMode.READ_ONLY)
+        .withClasspathResourceMapping(
+            "sharding.map",
+            "/usr/local/etc/haproxy/sharding.map",
+            BindMode.READ_WRITE)
+        .withExposedPorts(8400, 8401, 8404)
+        .withLogConsumer(new Slf4jLogConsumer(log, true).withPrefix("HAPROXY"));
   }
 
-  @Getter
-  @Setter
-  static class StatusTo
+  final GenericContainer createBackendContainer(String id)
   {
-    String status;
+    return new GenericContainer(DockerImageName.parse("juplo/chat-backend:0.0.1-SNAPSHOT"))
+      .withImagePullPolicy(NEVER_PULL)
+      .withNetwork(network)
+      .withNetworkAliases("backend-ID".replaceAll("ID", id))
+      .withCommand(Arrays.stream(getBackendCommand())
+          .map(commandPart -> commandPart.replaceAll("ID", id))
+          .toArray(size -> new String[size]))
+      .withExposedPorts(8080)
+      .waitingFor(Wait.forLogMessage(".*Started\\ ChatBackendApplication.*\\n", 1))
+      .withLogConsumer(new Slf4jLogConsumer(
+          log,
+          true
+          )
+          .withPrefix("BACKEND-ID".replaceAll("ID", id)));
   }
 }
--- src/test/java/de/juplo/kafka/chat/backend/KafkaHandoverITContainers.java
+++ src/test/java/de/juplo/kafka/chat/backend/KafkaHandoverITContainers.java
@@ -1,7 +1,8 @@
 package de.juplo.kafka.chat.backend;
 
 import lombok.extern.slf4j.Slf4j;
-import org.testcontainers.containers.*;
+import org.testcontainers.containers.Container;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
@@ -10,7 +11,7 @@ import java.io.IOException;
 
 
 @Slf4j
-class KafkaHandoverIT extends AbstractHandoverIT
+class KafkaHandoverITContainers extends AbstractHandoverITContainers
 {
   @Override
   void setUpExtra() throws IOException, InterruptedException
@@ -49,7 +50,7 @@ class KafkaHandoverIT extends AbstractHandoverIT
   }
 
 
-  KafkaContainer kafka =
+  private final KafkaContainer kafka =
       new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0"))
           .withNetwork(network)
           .withNetworkAliases("kafka")
@@ -58,49 +59,18 @@ class KafkaHandoverIT extends AbstractHandoverIT
           .waitingFor(Wait.forLogMessage(".*Kafka\\ Server\\ started.*\\n", 1))
           .withLogConsumer(new Slf4jLogConsumer(log, true).withPrefix("KAFKA"));
 
-  @Override
-  String[] getCommandBackend1()
-  {
-    return new String[]
-    {
-        "--chat.backend.instance-id=backend-1",
-        "--chat.backend.services=kafka",
-        "--chat.backend.kafka.bootstrap-servers=kafka:9999",
-        "--chat.backend.kafka.instance-uri=http://backend-1:8080",
-        "--chat.backend.kafka.num-partitions=10",
-        "--chat.backend.kafka.client-id-prefix=B1",
-        "--chat.backend.kafka.haproxy-runtime-api=haproxy:8401",
-        "--chat.backend.kafka.haproxy-map=/usr/local/etc/haproxy/sharding.map"
-    };
-  }
 
   @Override
-  String[] getCommandBackend2()
+  String[] getBackendCommand()
   {
     return new String[]
     {
-        "--chat.backend.instance-id=backend-2",
+        "--chat.backend.instance-id=backend_ID",
         "--chat.backend.services=kafka",
         "--chat.backend.kafka.bootstrap-servers=kafka:9999",
-        "--chat.backend.kafka.instance-uri=http://backend-2:8080",
+        "--chat.backend.kafka.instance-uri=http://backend-ID:8080",
         "--chat.backend.kafka.num-partitions=10",
-        "--chat.backend.kafka.client-id-prefix=B2",
-        "--chat.backend.kafka.haproxy-runtime-api=haproxy:8401",
-        "--chat.backend.kafka.haproxy-map=/usr/local/etc/haproxy/sharding.map"
-    };
-  }
-
-  @Override
-  String[] getCommandBackend3()
-  {
-    return new String[]
-    {
-        "--chat.backend.instance-id=backend-3",
-        "--chat.backend.services=kafka",
-        "--chat.backend.kafka.bootstrap-servers=kafka:9999",
-        "--chat.backend.kafka.instance-uri=http://backend-3:8080",
-        "--chat.backend.kafka.num-partitions=10",
-        "--chat.backend.kafka.client-id-prefix=B3",
+        "--chat.backend.kafka.client-id-prefix=BID",
         "--chat.backend.kafka.haproxy-runtime-api=haproxy:8401",
         "--chat.backend.kafka.haproxy-map=/usr/local/etc/haproxy/sharding.map"
     };
